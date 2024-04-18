26ba765c8656d006adb59a6563c5324280b3c5a4
KL_jiana <2436262516@qq.com>
2024-04-18 12:34:54 +0800

    update

--- kubejs/server_scripts/debug_modpack.js
+++ kubejs/server_scripts/debug_modpack.js
@@ -3,7 +3,7 @@
 
 // 一键获取箱子里的物品，快速打tag）
 PlayerEvents.chestClosed(event => {
-    event.inventory.allItems.forEach(item=>{
+    event.inventory.allItems.forEach(item => {
         event.server.tell(`event.add('extreme_hardcore:wood',${item.toItemString()})`)
     })
 })
