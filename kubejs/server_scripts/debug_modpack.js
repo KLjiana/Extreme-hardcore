d7193fa3bbb8f8601bcb6b065653c0394c6b039c
westernat <westernat233@163.com>
2024-05-09 11:11:09 +0800

    使用例

--- kubejs/server_scripts/debug_modpack.js
+++ kubejs/server_scripts/debug_modpack.js
@@ -1,4 +1,12 @@
 // packmode: debug
+
+const $Entity = Java.loadClass('net.minecraft.world.entity.Entity');
+/**
+ * @type {Internal.Class<Internal.Entity>}
+ */
+const entityClass = $Entity.__javaObject__;
+const portalTime = entityClass.getDeclaredField("f_19818_");
+
 // 写一些调试用的小工具（谁会想当码字苦力啊！
 
 // 一键获取箱子里的物品，快速打tag）
@@ -6,12 +14,5 @@ PlayerEvents.chestClosed(event => {
     event.inventory.allItems.forEach(item => {
         event.server.tell(`event.add('extreme_hardcore:wood',${item.toItemString()})`)
     })
+    console.log(portalTime.get(event.entity))
 })
-
-
-const $Entity = Java.loadClass('net.minecraft.world.entity.Entity');
-/**
- * @type {Internal.Class<Internal.Entity>}
- */
-const entityClass = $Entity.__javaObject__;
-const portalTime = entityClass.getDeclaredField("portalTime");
