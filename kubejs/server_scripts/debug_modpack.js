6f3964a8ef9a13a015998b33f3c382185c865a91
westernat <westernat233@163.com>
2024-05-09 11:31:26 +0800

    设置访问权限

--- kubejs/server_scripts/debug_modpack.js
+++ kubejs/server_scripts/debug_modpack.js
@@ -1,11 +1,12 @@
 // packmode: debug
 
-const $Entity = Java.loadClass('net.minecraft.world.entity.Entity');
+const $Entity = Java.loadClass('net.minecraft.world.entity.Entity'); // 反射Java类
 /**
  * @type {Internal.Class<Internal.Entity>}
  */
-const entityClass = $Entity.__javaObject__;
-const portalTime = entityClass.getDeclaredField("f_19818_");
+const entityClass = $Entity.__javaObject__; // 获取AccessibleObject
+const portalTime = entityClass.getDeclaredField('f_19818_'); // 获取'f_19818_'即'portalTime'字段
+portalTime.accessible = true; // 设置访问权限
 
 // 写一些调试用的小工具（谁会想当码字苦力啊！
 
@@ -14,5 +15,5 @@ PlayerEvents.chestClosed(event => {
     event.inventory.allItems.forEach(item => {
         event.server.tell(`event.add('extreme_hardcore:wood',${item.toItemString()})`)
     })
-    console.log(portalTime.get(event.entity))
+    console.log(portalTime.get(event.entity)) // 传入Entity实例以获取该实例的portalTime字段值
 })
