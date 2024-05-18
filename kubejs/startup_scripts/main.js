4998393335225087c241239d249acde00dc56e97
westernat <westernat233@163.com>
2024-05-18 08:06:51 -0300

    优化

--- kubejs/startup_scripts/main.js
+++ kubejs/startup_scripts/main.js
@@ -1,5 +1,5 @@
 ForgeModEvents.onEvent('net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent', event => {
-    registerFluidinteraction(Blocks.LAVA.fluid.fluidType,
+    registerFluidInteraction(Blocks.LAVA.fluid.fluidType,
         (level, currentPos, relativePos, currentState) => level.getBlockState(currentPos.below()).is(Blocks.BEDROCK),
         (level, currentPos, relativePos, currentState) => {
             let blockState = currentState.isSource() ? Blocks.DIAMOND_BLOCK.defaultBlockState() : Blocks.DIAMOND_ORE.defaultBlockState();
--- kubejs/startup_scripts/utils.js
+++ kubejs/startup_scripts/utils.js
@@ -9,16 +9,13 @@ function registerFluidInteraction(fluidType, hasFluidInteraction, fluidInteracti
     const InteractionInformation$Class = Java.loadClass('net.minecraftforge.fluids.FluidInteractionRegistry$InteractionInformation').__javaObject__;
     const HasFluidInteraction$Class = Java.loadClass('net.minecraftforge.fluids.FluidInteractionRegistry$HasFluidInteraction').__javaObject__;
     const FluidInteraction$Class = Java.loadClass('net.minecraftforge.fluids.FluidInteractionRegistry$FluidInteraction').__javaObject__;
-    //const Object$Class = Java.loadClass('java.lang.Object').__javaObject__;
-    const classLoader = FluidInteractionRegistry.__javaObject__.getClassLoader();
-    const Class$Class = classLoader.loadClass('java.lang.Class');
-    const Context$Class = classLoader.loadClass('dev.latvian.mods.rhino.Context');
-    const KubeJS$Class = classLoader.loadClass('dev.latvian.mods.kubejs.KubeJS');
-    const context = KubeJS$Class.getDeclaredMethod('getStartupScriptManager').invoke(null).context;
+    const Context = Java.loadClass('dev.latvian.mods.rhino.Context');
+    const KubeJS = Java.loadClass('dev.latvian.mods.kubejs.KubeJS');
+    /** @type {Internal.Context} */
+    const context = KubeJS.getStartupScriptManager().context;
     const InteractionInformation$Constructor = InteractionInformation$Class.getConstructor(HasFluidInteraction$Class, FluidInteraction$Class);
-    const Context$jsToJava = Context$Class.getDeclaredMethod('jsToJava', Context$Class, Object.__javaObject__, Class$Class);
     FluidInteractionRegistry.addInteraction(fluidType, InteractionInformation$Constructor.newInstance(
-        Context$jsToJava.invoke(null, context, hasFluidInteraction, HasFluidInteraction$Class),
-        Context$jsToJava.invoke(null, context, fluidInteraction, FluidInteraction$Class)
+        Context.jsToJava(context, hasFluidInteraction, HasFluidInteraction$Class),
+        Context.jsToJava(context, fluidInteraction, FluidInteraction$Class)
     ));
 }
