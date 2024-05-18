dec3815f57688fa324a1a3db052564516e99371d
westernat <westernat233@163.com>
2024-05-18 08:04:46 -0300

    解除

--- kubejs/startup_scripts/utils.js
+++ kubejs/startup_scripts/utils.js
@@ -9,14 +9,14 @@ function registerFluidInteraction(fluidType, hasFluidInteraction, fluidInteracti
     const InteractionInformation$Class = Java.loadClass('net.minecraftforge.fluids.FluidInteractionRegistry$InteractionInformation').__javaObject__;
     const HasFluidInteraction$Class = Java.loadClass('net.minecraftforge.fluids.FluidInteractionRegistry$HasFluidInteraction').__javaObject__;
     const FluidInteraction$Class = Java.loadClass('net.minecraftforge.fluids.FluidInteractionRegistry$FluidInteraction').__javaObject__;
-    const Object$Class = Java.loadClass('java.lang.Object').__javaObject__;
+    //const Object$Class = Java.loadClass('java.lang.Object').__javaObject__;
     const classLoader = FluidInteractionRegistry.__javaObject__.getClassLoader();
     const Class$Class = classLoader.loadClass('java.lang.Class');
     const Context$Class = classLoader.loadClass('dev.latvian.mods.rhino.Context');
     const KubeJS$Class = classLoader.loadClass('dev.latvian.mods.kubejs.KubeJS');
     const context = KubeJS$Class.getDeclaredMethod('getStartupScriptManager').invoke(null).context;
     const InteractionInformation$Constructor = InteractionInformation$Class.getConstructor(HasFluidInteraction$Class, FluidInteraction$Class);
-    const Context$jsToJava = Context$Class.getDeclaredMethod('jsToJava', Context$Class, Object$Class, Class$Class);
+    const Context$jsToJava = Context$Class.getDeclaredMethod('jsToJava', Context$Class, Object.__javaObject__, Class$Class);
     FluidInteractionRegistry.addInteraction(fluidType, InteractionInformation$Constructor.newInstance(
         Context$jsToJava.invoke(null, context, hasFluidInteraction, HasFluidInteraction$Class),
         Context$jsToJava.invoke(null, context, fluidInteraction, FluidInteraction$Class)
