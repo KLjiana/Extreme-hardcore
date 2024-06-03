472d8e100e545344db07823e9ca06a2dc75f7149
westernat <westernat233@163.com>
2024-06-03 17:04:28 +0800

    更优的方案

--- kubejs/startup_scripts/utils.js
+++ kubejs/startup_scripts/utils.js
@@ -9,13 +9,10 @@ function registerFluidInteraction(fluidType, hasFluidInteraction, fluidInteracti
     const InteractionInformation$Class = Java.loadClass('net.minecraftforge.fluids.FluidInteractionRegistry$InteractionInformation').__javaObject__;
     const HasFluidInteraction$Class = Java.loadClass('net.minecraftforge.fluids.FluidInteractionRegistry$HasFluidInteraction').__javaObject__;
     const FluidInteraction$Class = Java.loadClass('net.minecraftforge.fluids.FluidInteractionRegistry$FluidInteraction').__javaObject__;
-    const Context = Java.loadClass('dev.latvian.mods.rhino.Context');
-    const KubeJS = Java.loadClass('dev.latvian.mods.kubejs.KubeJS');
-    /** @type {Internal.Context} */
-    const context = KubeJS.getStartupScriptManager().context;
+    const UtilsJS = Java.loadClass('dev.latvian.mods.kubejs.util.UtilsJS');
     const InteractionInformation$Constructor = InteractionInformation$Class.getConstructor(HasFluidInteraction$Class, FluidInteraction$Class);
     FluidInteractionRegistry.addInteraction(fluidType, InteractionInformation$Constructor.newInstance(
-        Context.jsToJava(context, hasFluidInteraction, HasFluidInteraction$Class),
-        Context.jsToJava(context, fluidInteraction, FluidInteraction$Class)
+        UtilsJS.makeFunctionProxy('startup', HasFluidInteraction$Class, hasFluidInteraction),
+        UtilsJS.makeFunctionProxy('startup', FluidInteraction$Class, fluidInteraction)
     ));
 }
